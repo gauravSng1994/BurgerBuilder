8995ec2d8ea94e115c86938537637db879af5bd2
Gaurav Kumar <gauravsng1994@gmail.com>
2020-04-05 16:53:54 +0530

    ingredients' prices added

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../hoc/Aux'
 import Burger from '../components/Burger/Burger'
 import BuildControls from '../components/Burger/BuildControls/BuildControls'
 
+const INGREDIENT_PRICES = {
+    salad : 5,
+    cheese: 10,
+    meat: 20,
+    bacon: 15
+};
+
 class BurgerBuilder extends Component{
 
     state = {
@@ -11,7 +18,17 @@ class BurgerBuilder extends Component{
             bacon:0,
             cheese:0,
             meat:0
-        }
+        },
+        totalPrice:20
+    };
+
+    addIngredientHandler = (type)=>{
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
     };
     render(){
         return (
